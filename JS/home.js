772a17bbe2219ae9e2b9a573dc6479d78258b13e
Wendell Moreira Guindani <113864252+Wendell9@users.noreply.github.com>
2024-10-01 17:27:37 -0300

    First Commit

---
+++ JS/home.js
@@ -0,0 +1,28 @@
+document.querySelector("#form-busca").onsubmit = function () {
+  if (document.querySelector("#q").value == "") {
+    document.querySelector("#form-busca").style.background = "red";
+    return false;
+  }
+};
+
+var banners = ["IMG/destaque-home.png", "img/destaque-home-2.png"];
+var bannerAtual = 0;
+var timer = setInterval(trocaBanner, 4000);
+var controle = document.querySelector(".pause");
+
+function trocaBanner() {
+  bannerAtual = (bannerAtual + 1) % 2;
+  document.querySelector(".destaque img").src = banners[bannerAtual];
+}
+timer;
+
+controle.onclick = function () {
+  if (controle.className == "pause") {
+    clearInterval(timer);
+    controle.className = "play";
+  } else {
+    timer = setInterval(trocaBanner, 4000);
+    controle.className = "pause";
+  }
+  return false;
+};
